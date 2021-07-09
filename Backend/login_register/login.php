9240c7981f9606e3bad998e34883514a193d3d7f
Alexandru Cristian <76886047+Istentel@users.noreply.github.com>
2021-07-09 15:01:49 +0300

    login/register submit
    added a security if/else statement

--- Backend/login_register/login.php
+++ Backend/login_register/login.php
@@ -1,61 +1,66 @@
 <?php
-    #1.Get data from site
-     $email = filter_input(INPUT_POST, "email");
-     $password = filter_input(INPUT_POST, "password");
+    if(isset($_POST["submit"])){
+        #1.Get data from site
+        $email = filter_input(INPUT_POST, "email");
+        $password = filter_input(INPUT_POST, "password");
 
-    #2.Get data from database
-    require('../connect.php');
+        #2.Get data from database
+        require('../connect.php');
 
-    #Define query
-    try{
-        $query_data = 'SELECT * FROM accounts WHERE email=:email';
+        #Define query
+        try{
+            $query_data = 'SELECT * FROM accounts WHERE email=:email';
 
-        #Prepare statement to execute
+            #Prepare statement to execute
 
-        #This creates a PDOStatement object
-        $account_statement = $db->prepare($query_data);
+            #This creates a PDOStatement object
+            $account_statement = $db->prepare($query_data);
 
-        #Bind email to the query
-        $account_statement->bindValue(':email', $email);
+            #Bind email to the query
+            $account_statement->bindValue(':email', $email);
 
-        #Execute the query
-        $account_statement->execute();
-    }catch(PDOException $e){
-        $err_msg = $e->getMessage();
-        include('../error.php');
-        exit;
-    }
+            #Execute the query
+            $account_statement->execute();
+        }catch(PDOException $e){
+            $err_msg = $e->getMessage();
+            include('../error.php');
+            exit;
+        }
 
-    #Return the password
-    $db_data = $account_statement->fetchAll();
-    
-    #Allow new sql statements to execute
-    $account_statement->closeCursor();
+        #Return the password
+        $db_data = $account_statement->fetchAll();
+        
+        #Allow new sql statements to execute
+        $account_statement->closeCursor();
 
-    #Check if there is data 
-    if(!$db_data){
-        $err_msg = "Email or password are invalid!";
-        include('../error.php');
-        exit;
-    }
+        #Check if there is data 
+        if(!$db_data){
+            $err_msg = "Email or password are invalid!";
+            include('../error.php');
+            exit;
+        }
 
-    #3.Compare data
+        #3.Compare data
 
-    if(password_verify($password, $db_data[0]["password"])){
-        #Start session
-        session_start();
+        if(password_verify($password, $db_data[0]["password"])){
+            #Start session
+            session_start();
 
-        #Set variables
-        $_SESSION["fname"] = $db_data[0]["first_name"];
-        $_SESSION["lname"] = $db_data[0]["last_name"];
-        $_SESSION["email"] = $db_data[0]["email"];
+            #Set variables
+            $_SESSION["fname"] = $db_data[0]["first_name"];
+            $_SESSION["lname"] = $db_data[0]["last_name"];
+            $_SESSION["email"] = $db_data[0]["email"];
 
-        #Exist this page and redirect to new page
-        header("Location: session.php");
-		exit;
+            #Exist this page and redirect to new page
+            header("Location: session.php");
+            exit;
+        }else{
+            $err_msg = "Password doesn't match!";
+            include('../error.php');
+        }
     }else{
-        $err_msg = "Password doesn't match!";
-        include('../error.php');
+        header("Location: login.html");
+        exit;
     }
 
 ?>
